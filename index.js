ce0b133ca40d91767b6ae4677b6f0bc912bbed56
mverlizzophoto@gmail.com <mverlizzophoto@gmail.com>
2018-07-10 01:55:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-var name = "susan"
+var name = "Joe"
 var height = "74"
-if (name === "susan") {
-  console.log("The name is correct")
-} else {
-  console.log("Expected the name to be susan")
-}
-
 
 // Don't worry about this
 module.exports = { name, height
